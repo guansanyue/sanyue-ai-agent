aab6491f301b12021e465fe965889a8a58f6adbf
guansanyue <2369401938@qq.com>
2025-12-14 21:39:21 +0800

    1）项目初始化 2）Spring AI的基础使用 2.1）ChatModel 2.2）ChatClient 2.3）自定义 Advisors 顾问机制（日志拦截器） 2.4）多轮对话实现（基于内存） 2.5）记忆持久化（基于文件读写） 2.6）结构化输出

---
+++ src/main/java/com/sanyue/sanyueaiagent/SanyueAiAgentApplication.java
@@ -0,0 +1,13 @@
+package com.sanyue.sanyueaiagent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SanyueAiAgentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SanyueAiAgentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sanyue/sanyueaiagent/SanyueAiAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sanyue.sanyueaiagent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SanyueAiAgentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
